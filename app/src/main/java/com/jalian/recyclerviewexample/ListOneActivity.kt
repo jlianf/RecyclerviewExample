19c93df0a4f8fe1071fb740f9fefe5b70bdf20a8
jlianf <jalianfebriandy15@gmail.com>
2023-03-15 13:50:33 +0700

    Set List Item

---
+++ app/src/main/java/com/jalian/recyclerviewexample/ListOneActivity.kt
@@ -0,0 +1,12 @@
+package com.jalian.recyclerviewexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListOneActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView()
+    }
+}
--- app/src/main/java/com/jalian/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/jalian/recyclerviewexample/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.jalian.recyclerviewexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jalian.recyclerviewexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnOne.setOnClickListener{
+            val intent = Intent(this, ListOneActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
